8610db6af0962224536f08e099e8ed1a4b8fa438
Jrodr4544 <jelson.rodriguez@gmail.com>
2016-10-16 16:52:42 -0400

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,33 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, FavoriteFood;
+  var people = [
+    {
+      name: "John Doe",
+      favoriteFood: "pizza"
+    },
+    {
+      name: "Mike None",
+      favoriteFood: "sushi"
+    },
+    {
+      name: "Bill Gates",
+      favoriteFood: "sushi"
+    }
+  ];
 
-	beforeEach(module('app'));
+	// beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
-	}));
+	beforeEach(function(){
+    module.apply('app');
 
+    inject(function($injector){
+      filter = $injector.get('$filter')('FavoriteFood');
+    });
+  });
+
+  it('should filter on favorite food', function() {
+    // var result = $filter('FavoriteFood')('sushi', people)
+    expect(filter(people, 'sushi')).toBe('sushi');
+  })
 	
 });
